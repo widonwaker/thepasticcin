ea5f49a59289dea64da32f58f93359eefa43515b
widonwaker <hey.lol@live.it>
2018-05-15 16:41:19 +0200

    Update ita.js

--- js/ita.js
+++ js/ita.js
@@ -1,4 +1,5 @@
 if (document.getElementById("play") !== null) { document.getElementById("play").innerHTML = 'Gioca'; }
 function loadRibbon() {
    $('.ribbon').attr('src', 'img/panel/alloggi_ita.png'); 
+   alert('x');
 }
