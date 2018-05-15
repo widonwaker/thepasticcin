dd4c8cf47ee7a25af38c8d3d4b88193159186c31
widonwaker <hey.lol@live.it>
2018-05-15 16:41:37 +0200

    Update eng.js

--- js/eng.js
+++ js/eng.js
@@ -1,4 +1,5 @@
 if (document.getElementById("play") !== null) { document.getElementById("play").innerHTML = 'Play'; }
 function loadRibbon() {
    $('.ribbon').attr('src', 'img/panel/alloggi_eng.png'); 
+   alert('x');
 }
