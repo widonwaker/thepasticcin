77e7ea369ede1c3f24bf75f21f275bcdb57e952a
widonwaker <hey.lol@live.it>
2018-05-22 17:05:37 +0200

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,11 @@
 // FUNZIONI upgrades
 function CheckUpgradeAvailable(panel) {
 	var divs = document.getElementById(panel).getElementsByClassName("skills");
+	var id = document.getElementById(panel);
+	var newimg = id.getElementsByClassName("upgrade")[0].querySelector('img');
     for(var i = 0; i < divs.length; i++) {
 		if (divs[i].src.indexOf('skills_5') === -1) break;
 		if (i === divs.length-1) { 
-		    var id = document.getElementById(panel);
-			var newimg = id.getElementsByClassName("upgrade")[0].querySelector('img');
 			newimg.src="img/btn_green.png";
 			id.getElementsByClassName("upgrade")[0].classList.remove("grey");
 			id.getElementsByClassName("upgrade")[0].classList.add("green");
@@ -15,6 +15,20 @@ function CheckUpgradeAvailable(panel) {
 		}
     }
 	return false;
+}
+
+function ClearUpgrade (id) {
+	var resetskills = id.parentElement.parentElement.getElementsByClassName("skills");
+	for(var i = 0; i < resetskills.length; i++) {
+	        resetskills[i].src = 'img/panel/skills_0.png';
+	    }
+	localStorage.setItem("energyskills",0);	
+	localStorage.setItem("machineryskills",0);	
+	id.parentElement.querySelector('img').src="img/panel/btn_locked.png";
+	id.parentElement.classList.remove("green");
+	id.parentElement.classList.add("grey");
+	id.style.top="55%";
+	id.style.left="52%";
 }
 
 
@@ -29,6 +43,7 @@ function CheckSingleUpgrade(elem) {
 		elem.parentElement.classList.remove("grey-soft");
 	}
 	CheckUpgradeAvailable(elem.parentElement.parentElement.parentElement.id);
+	localStorage.setItem("money_save",start);
 }
 
 
@@ -43,8 +58,8 @@ function FactoryPanel() {
 		$('#factory-rib').attr('src', 'img/panel/fabbrica_ita.png'); 
 	}
 	//CheckUpgradeAvailable("factory-panel");
-	document.getElementById("power").getElementsByClassName("title")[0].innerHTML = power_title;
-	document.getElementById("power").getElementsByClassName("desc")[0].innerHTML = '+'+localStorage.getItem("energy")+'% '+power_desc;
+	document.getElementById("power").getElementsByClassName("title")[0].innerHTML = energy_title;
+	document.getElementById("power").getElementsByClassName("desc")[0].innerHTML = '+'+localStorage.getItem("energy")+'% '+energy_desc;
 	document.getElementById("power").getElementsByClassName("skills")[0].src = 'img/panel/skills_'+localStorage.getItem("energyskills")+'.png';
 	document.getElementById("power").getElementsByClassName("btn-cost")[0].innerHTML = localStorage.getItem("energy_val");
 	
@@ -74,19 +89,28 @@ function SingleUpgrade(elem, skill) {
 		var perc = parseInt(localStorage.getItem(skill));
 		localStorage.setItem(skill,perc+2); // setta nuovo valore upgrade
 		var desc = elem.parentElement.parentElement.getElementsByClassName("desc")[0];
-		desc.innerHTML = '+'+localStorage.getItem(skill)+'% '+power_desc;
+		desc.innerHTML = '+'+localStorage.getItem(skill)+'% '+window[skill+'_desc'];
         speed = (parseInt(localStorage.getItem("fabbrica"))-parseInt(localStorage.getItem("orsettini")) )*(1-(parseInt(localStorage.getItem("energy")) / 100))*(1-(parseInt(localStorage.getItem("machinery")) / 100))*(1);
+		document.getElementById("skillup").play();
 	}
 	CheckSingleUpgrade(elem);
 }
 
 
-function FactoryNext(elem) {
-	var num = document.getElementById("factory").src.match(/\d+/g).map(Number);
-	console.log(num[0]);
+function BuildingNext(elem) {
+	var mainid = elem.parentElement.parentElement.id;
+	var matches = mainid.match(/\S+(?=-)/g);
+	var num = document.getElementById(matches).src.match(/\d+/g).map(Number);
 	if(CheckUpgradeAvailable(elem.parentElement.parentElement.id)===true && num[0]!==5) {    
 	    num = num[0]+1;
-		localStorage.setItem("factory","img/edifici/fabbrica"+num+".png");
-	    document.getElementById("factory").src ="img/edifici/fabbrica"+num+".png";
-	}
+		localStorage.setItem(matches,"img/edifici/"+matches+num+".png");
+	    document.getElementById(matches).src ="img/edifici/"+matches+num+".png";
+		document.getElementById("levelup").play();
+		ClearUpgrade(elem);
+		$('#'+mainid).hide();
+		$('#'+matches+'levelup').show();
+	        setTimeout(function() {
+                $('#'+matches+'levelup').fadeOut();
+            }, 1000);
+	    }
 }
