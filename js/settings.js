faa20c4318c922ca2105905ec8d1f8d7aa9eac69
widonwaker <hey.lol@live.it>
2018-05-26 20:21:35 +0200

    Add files via upload

---
+++ js/settings.js
@@ -0,0 +1,13 @@
+if (localStorage.getItem("factory")===null) localStorage.setItem("factory","img/edifici/factory1.png");
+if (localStorage.getItem("money_save")===null || localStorage.getItem("money_save")=="NaN") localStorage.setItem("money_save",3000);
+if (localStorage.getItem("pasticcino")===null) localStorage.setItem("pasticcino",1);
+if (localStorage.getItem("fabbrica")===null) localStorage.setItem("fabbrica",1000);
+if (localStorage.getItem("orsettini")===null) localStorage.setItem("orsettini",1);
+if (localStorage.getItem("energyskills")===null) localStorage.setItem("energyskills",0);
+if (localStorage.getItem("machineryskills")===null) localStorage.setItem("machineryskills",0);
+if (localStorage.getItem("energy")===null) localStorage.setItem("energy",0);	
+if (localStorage.getItem("energy_val")===null) localStorage.setItem("energy_val",30);
+if (localStorage.getItem("machinery")===null) localStorage.setItem("machinery",0);	
+if (localStorage.getItem("machinery_val")===null) localStorage.setItem("machinery_val",20);
+if (localStorage.getItem("bgmusic")===null) localStorage.setItem("bgmusic","On");
+if (localStorage.getItem("sound")===null) localStorage.setItem("sound","On");
